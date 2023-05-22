b4e4451eacde1f0e3a1b700555e71dd2bff41cf7
MaxOnn10 <maks.melenevskiy@gmail.com>
2023-05-22 23:36:24 +0300

    main page: gallary, footer, adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,83 @@ burgerMenu.addEventListener('click', () => {
     burgerContainer.classList.toggle('header__burger-container--active');
     nav.classList.toggle('header__nav-active');
 });
+
+// Fancybox.show([{ src: "#dialog-content", type: "inline" }]);
+Fancybox.bind("[data-fancybox]", {
+    autoFocus: false,
+    dragToClose: false,
+    groupAttr: false
+});
+
+// inputmask
+// const form = document.querySelector('.form');
+// const telSelector = form.querySelector('input[type="tel"]');
+// const inputMask = new Inputmask('+1 (999) 999-9999');
+// inputMask.mask(telSelector);
+
+// const validation = new JustValidate('.form');
+
+// validation
+//   .addField('.input-name', [
+//     {
+//       rule: 'minLength',
+//       value: 2,
+//     },
+//     {
+//       rule: 'maxLength',
+//       value: 30,
+//     },
+//     {
+//       rule: 'required',
+//       value: true,
+//       errorMessage: 'Enter a first name!'
+//     }
+//   ])
+//   .addField('.input-mail', [
+//     {
+//       rule: 'required',
+//       value: true,
+//       errorMessage: 'Email is required',
+//     },
+//     {
+//       rule: 'email',
+//       value: true,
+//       errorMessage: 'Enter a valid Email',
+//     },
+//   ])
+//   .addField('.input-tel', [
+//     {
+//       rule: 'required',
+//       value: true,
+//       errorMessage: 'Phone is required',
+//     },
+//     {
+//       rule: 'function',
+//       validator: function() {
+//         const phone = telSelector.inputmask.unmaskedvalue();
+//         return phone.length === 10;
+//       },
+//       errorMessage: 'Enter the correct phone number',
+//     },
+//   ]).onSuccess((event) => {
+//     console.log('Validation passes and form submitted', event);
+
+//     let formData = new FormData(event.target);
+
+//     console.log(...formData);
+
+//     let xhr = new XMLHttpRequest();
+
+//     xhr.onreadystatechange = function () {
+//       if (xhr.readyState === 4) {
+//         if (xhr.status === 200) {
+//           console.log('Send');
+//         }
+//       }
+//     }
+
+//     xhr.open('POST', 'mail.php', true);
+//     xhr.send(formData);
+
+//     event.target.reset();
+//   });
