ee75a172f2a30cf3283359bd64ab861ca3f76d26
MaxOnn10 <maks.melenevskiy@gmail.com>
2023-06-12 22:24:29 +0300

    fix nav sub menu for safari, change headers to standard font

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,32 @@
 const burgerMenu = document.querySelector('.header__burger');
 const burgerContainer = document.querySelector('.header__burger-container');
-const nav = document.querySelector('.header__nav')
-// const photo = document.querySelector('.photo')
+const nav = document.querySelector('.header__nav');
+const storiesLink = document.querySelector('.header__menu-stories');
+const submenu = document.querySelector('.header__nav-submenu');
+
+// Burger
 burgerMenu.addEventListener('click', () => {
     burgerMenu.classList.toggle('header__burger-active');
     burgerContainer.classList.toggle('header__burger-container--active');
     nav.classList.toggle('header__nav-active');
-});
+    submenu.classList.remove('submenu-visible'); // Close submenu on burgerMenu click
+  });
+  
+  // Stories nav menu
+  storiesLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    submenu.classList.toggle('submenu-visible'); // Toggle submenu visibility
+  });
 
- //download-lock
+
+// Close submenu on burgerMenu click
+// burgerMenu.addEventListener('click', () => {
+//   submenu.style.visibility = 'hidden';
+//   submenu.style.opacity = '0';
+//   submenu.style.transform = 'rotateX(-90deg)';
+// });
+
+//download-lock
 const photos = document.querySelectorAll('.photo');
 photos.forEach(photo => {
     photo.addEventListener('contextmenu', (event) => {
